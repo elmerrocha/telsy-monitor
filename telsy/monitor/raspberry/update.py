5473a8e464b8666033cab945bfa448f6079e3e10
Elmer Rocha <elmer.rocha.j@gmail.com>
2022-03-28 16:44:48 -0500

    Get remote version

--- telsy/monitor/raspberry/update.py
+++ telsy/monitor/raspberry/update.py
@@ -11,13 +11,13 @@ from time import sleep
 
 URI = 'https://github.com/elmerrocha/telsy-monitor.git'
 # URI = 'origin'
-LOCAL_VERSION  = getoutput('git rev-parse HEAD')
-REMOTE_VERSION = getoutput('git ls-remote '+URI).split('\t')[0]
-UPDATABLE = LOCAL_VERSION!=REMOTE_VERSION
 
 def update_firmware():
     ''' Update firmware searching last commit '''
-    if UPDATABLE:
+    local_version  = getoutput('git rev-parse HEAD')
+    remote_version = getoutput('git ls-remote '+URI).split('\t')[0]
+    updatable = local_version!=remote_version
+    if updatable:
         print("Different version")
         # system('git reset --hard')
         system('git pull origin')
@@ -27,11 +27,14 @@ def update_firmware():
 
 def update_info():
     ''' Return git version information '''
+    local_version  = getoutput('git rev-parse HEAD')
+    remote_version = getoutput('git ls-remote '+URI).split('\t')[0]
+    updatable = local_version!=remote_version
     info = {
-            'updatable': UPDATABLE,
-            'localVersion': LOCAL_VERSION[0:10],
+            'updatable': updatable,
+            'localVersion': local_version[0:10],
             'localDate': getoutput('git show -s --format=%cD')[0:25],
-            'remoteVersion': REMOTE_VERSION[0:10],
+            'remoteVersion': remote_version[0:10],
             'remoteDate': getoutput('git log -1 --format=%cD origin')[0:25]
         }
     return info
